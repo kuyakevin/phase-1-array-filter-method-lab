50ee487fc5470c50d1ed04e9c116d65d839c243f
Kevin Chaney <kevin_chaney@icloud.com>
2022-05-14 00:47:45 -0700

    commit tree b21902f6640fbbf1d23921b2e61dd156e0f13ad3

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+const findMatching = (drivers, query) => {
+    return drivers.filter(name => name.toLowerCase().indexOf(query.toLowerCase()) !==-1)
+}
+
+const fuzzyMatch = (drivers, query) => {
+    return drivers.filter(name => name.startsWith(query)) 
+}
+
+const matchName = (drivers, querty) => {
+    return drivers.filter(names => names.name === querty)
+}
